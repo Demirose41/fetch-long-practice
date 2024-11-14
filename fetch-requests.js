98aa3b5e4c2f1fbd8e22c5d577860d8a8efdc518
demirose41 <demirose41@gmail.com>
2024-11-13 20:47:03 -0800

    completed practice

--- fetch-requests.js
+++ fetch-requests.js
@@ -5,16 +5,43 @@ Paste your code for fetch requests here once you finish each phase.
 
 /* ============================== Phase 1 ============================== */
 
-// Your code here
+fetch("/products", {
+    method : "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: "name=pop&description=good+taste&categories=grocery&price=9.99"
+})
 
 
 
 /* ============================== Phase 2 ============================== */
 
 // Your code here
+const request = await fetch("/products", {
+    method : "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: "name=pop&description=good+taste&categories=grocery&price=9.99"
+})
 
-
+request.url
+request.status
+request.headers.get("content-type")
 
 /* ============================== Phase 3 ============================== */
 
 // Your code here
+const request = await fetch("/products", {
+    method : "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: new URLSearchParams({
+  name: "Caribbean Delight Coffee",
+  description: "Made by Manatee Coffee",
+  price: 11.99,
+  categories: "grocery"
+})
+})
